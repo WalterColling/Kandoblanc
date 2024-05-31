e8c8178ea7d4f2dfb195ec1da949003b4382c84e
walcoll <hello@walcoll.com>
2024-05-31 12:20:59 +0100

    model and material update

--- src/introMain.jsx
+++ src/introMain.jsx
@@ -3,16 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { Canvas } from "@react-three/fiber";
 import Intro from "./Intro";
-import { Perf } from "r3f-perf";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Canvas
-    // shadows
+    shadows
     dpr={(0.5, 2)}
     camera={{ position: [0.5, 0.2, 1], fov: 30, near: 0.1, far: 35 }}
   >
-    <Perf position="top-left" />
-
     <Suspense fallback={null}>
       <Intro />
     </Suspense>
--- src/objectMain.jsx
+++ src/objectMain.jsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { Canvas } from "@react-three/fiber";
 import ObjectPage from "./ObjectPage";
-import { Perf } from "r3f-perf";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Canvas
@@ -11,8 +10,6 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     dpr={[2, 4]}
     camera={{ position: [0.5, 0.2, 5], fov: 30, near: 0.1, far: 35 }}
   >
-    <Perf position="top-left" />
-
     <Suspense fallback={null}>
       <ObjectPage />
     </Suspense>
