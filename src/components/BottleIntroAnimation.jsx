456f13a7d25a5388a55f0b1cd615c06eadeeba54
walcoll <hello@walcoll.com>
2024-07-01 12:20:48 +0100

    delay added

--- src/components/BottleIntroAnimation.jsx
+++ src/components/BottleIntroAnimation.jsx
@@ -26,50 +26,55 @@ export function BottleIntro({ ...props }) {
       const modelModule = await import("./Model");
       setModel(() => modelModule.default);
     })();
+  }, []);
 
+  useEffect(() => {
     if (isModelLoaded) {
-      // All models and materials are loaded, set the context to hide the overlay
-      setObjectLoaded(true);
+      // Introduce a small delay before setting the context to hide the overlay
+      gsap.delayedCall(0.0, () => {
+        // Adjust the delay time as needed
+        setObjectLoaded(true);
 
-      // Introduce a small delay before starting the initial animation
-      gsap.delayedCall(0.2, () => {
-        // Start the initial animation
-        if (topRef.current) {
-          gsap.from([topRef.current.position], {
-            duration: 4.5,
-            y: 0.6,
-            ease: "power3.out",
-            delay: 0.2,
-          });
-        }
+        // Introduce a small delay before starting the initial animation
+        gsap.delayedCall(0.2, () => {
+          // Start the initial animation
+          if (topRef.current) {
+            gsap.from([topRef.current.position], {
+              duration: 4.5,
+              y: 0.6,
+              ease: "power3.out",
+              delay: 0.2,
+            });
+          }
 
-        if (neckRef.current) {
-          gsap.from([neckRef.current.position], {
-            duration: 4.2,
-            y: 0.3,
-            ease: "power3.out",
-            delay: 0.2,
-          });
-        }
+          if (neckRef.current) {
+            gsap.from([neckRef.current.position], {
+              duration: 4.2,
+              y: 0.3,
+              ease: "power3.out",
+              delay: 0.2,
+            });
+          }
 
-        if (obj.current) {
-          gsap.from([obj.current.position], {
-            duration: 5,
-            x: -0.05,
-            y: 0.2,
-            z: 0.6,
-            ease: "power3.out",
-          });
+          if (obj.current) {
+            gsap.from([obj.current.position], {
+              duration: 5,
+              x: -0.05,
+              y: 0.2,
+              z: 0.6,
+              ease: "power3.out",
+            });
 
-          gsap.from([obj.current.rotation], {
-            duration: 5.2,
-            y: -3,
-            ease: "power3.out",
-          });
-        }
+            gsap.from([obj.current.rotation], {
+              duration: 5.2,
+              y: -3,
+              ease: "power3.out",
+            });
+          }
 
-        // Trigger the second animation after the initial one is complete
-        setAnimationTriggered(true);
+          // Trigger the second animation after the initial one is complete
+          setAnimationTriggered(true);
+        });
       });
     }
   }, [isModelLoaded, setObjectLoaded]);
